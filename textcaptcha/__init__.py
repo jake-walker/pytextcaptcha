aed3bda53512a3e0b72c54b77a7ebb628e6bfbf3
Jake Walker <hi@jakew.me>
2020-01-02 21:08:30 +0000

    Add Drone Config

--- textcaptcha/__init__.py
+++ textcaptcha/__init__.py
@@ -1,4 +1,6 @@
-__version__ = '0.1.0'
+"""Main textcaptcha package."""
+
+__version__ = '0.1.1'
 
 from .captcha import Captcha  # noqa: F401
 from .captcha_fetcher import CaptchaFetcher  # noqa: F401
